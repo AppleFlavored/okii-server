6fad7857d87487d497d654c17d9a5551bab99c57
AppleFlavored <boostelixir@gmail.com>
2022-06-04 22:53:08 -0400

    Refactoring for fun

--- src/server.js
+++ src/server.js
@@ -1,73 +1,56 @@
+// @ts-check
+
 const moniker = require('moniker');
 const express = require('express');
-const { Server } = require('ws'); 
+const { Server } = require('ws');
 
-// HTTP server
-const PORT = process.env.PORT || 3000;
-const server = express()
-    .use((req, res) => res.send('bruh'))
-    .listen(PORT, console.log(`Listening on port ${PORT}`));
+class ConnectionManager {
+    constructor() {
+        this.clients = new Array();
+    }
 
-// WebSocket server
-const wss = new Server({ server });
-
-const chatManager = {
-    clients: new Array(),
-
-    start: function() {
+    start() {
+        const wss = new Server();
         wss.on('connection', socket => {
-            socket.on('message', message => {
-                this.handlePacket(socket, message);
-            });
-
+            socket.on('message', message => this.handlePacket(socket, message));
             socket.on('close', () => {
                 this.removeClient(socket);
                 this.updateUserList();
             });
-        });
-    },
+        })
+    }
 
-    // TODO: Custom nicknames?
-    registerClient: function(ws) {
-        const client = { socket: ws, nickname: moniker.choose() };
-
-        this.clients.push(client);
-        this.updateUserList();
+    registerClient(ws) {
+        const client = {
+            socket: ws,
+            nickname: moniker.choose()
+        };
 
         console.log(`${client.nickname} connected.`);
-    },
+        this.clients.push(client);
+        this.updateUserList();
+    }
 
-    removeClient: function(ws) {
-        this.clients.forEach((v, i) => {
-            if (v.socket === ws) {
-                console.log(`${v.nickname} disconnected.`)
-                this.clients.splice(i, 1);
-            }
-        })
-    },
-
-    handlePacket: async function(ws, message) {
-        const split = message.split('\n');
-
-        console.log(split);
-
-        switch (split[0]) {
+    handlePacket(ws, message) {
+        const data = message.split('\n');
+        switch (data[0]) {
             case 'CONNECT':
                 this.registerClient(ws);
-                ws.send(`OK\n`);
+                ws.send('OK\n');
                 break;
             case 'SEND':
-                if (split[1].trim() === '')
+                // I'm not refactoring this. /shrug
+                if (data[1].trim().length === 0)
                     break;
 
                 this.clients.forEach(client => {
-                    const response = { author: this.getNickname(ws), content: escape(split[1]) };
+                    const response = { author: this.getNickname(ws), content: encodeURI(data[1]) };
                     //console.log(response);
                     client.socket.send(`MESSAGE\n${JSON.stringify(response)}`)
                 });
 
-                if (split[1].startsWith('!')) {
-                    const args = split[1].content.slice('!'.length).trim().split(/ +/);
+                if (data[1].startsWith('!')) {
+                    const args = data[1].content.slice('!'.length).trim().split(/ +/);
 	                const command = args.shift().toLowerCase();
 
                     switch (command) {
@@ -76,39 +59,41 @@ const chatManager = {
                             break;
                     }
                 }
-
                 break;
             default:
                 break;
         }
-    },
+    }
 
-    changeNickname: function(ws, nickname) {
-        for(i = 0; i < this.clients.length; i++){
-			if(this.clients[i].socket === ws){
-				this.clients[i].nickname = nickname;
-                this.updateUserList();
-			}
-		}
-    },
+    updateUserList() {
+        const data = this.clients.map(c => c.nickname);
+        this.clients.forEach(c => c.socket.send(`LIST\n${JSON.stringify(data)}`));
+    }
 
-    getNickname: function(ws)  {
-        for(i = 0; i < this.clients.length; i++){
-			if(this.clients[i].socket === ws){
-				return this.clients[i].nickname;
-			}
-		}
-    },
+    changeNickname(ws, nickname) {
+        let client = this.clients.find(c => c.socket === ws);
+        if (client === undefined) return;
 
-    updateUserList: function() {
-        var userList = new Array();
-		for(i = 0; i < this.clients.length; i++){
-			userList.push(this.clients[i].nickname);
-		}
-        this.clients.forEach(client => {
-            client.socket.send(`LIST\n${JSON.stringify(userList)}`);
-        });
-    },
-};
+        client.nickname = nickname;
+        this.updateUserList();
+    }
 
-chatManager.start();
+    getNickname(ws) {
+        return this.clients.find(c => c.socket === ws).nickname;
+    }
+
+    removeClient(ws) {
+        let idx;
+        const client = this.clients.find((c, i) => {idx = i; return c.socket === ws});
+        if (client === undefined) return;
+
+        console.log(`${client.nickname} disconnected.`)
+        this.clients.splice(idx, 1);
+    }
+}
+
+const PORT = process.env.PORT || 3000;
+const _ = express().listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+const connManager = new ConnectionManager();
+connManager.start();
